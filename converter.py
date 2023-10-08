8a980aecbc63f06f31ad39f1a70fb6140d03e6f2
giwon2004 <giwons04@naver.com>
2023-10-08 13:27:37 +0900

    data upload

---
+++ converter.py
@@ -0,0 +1,42 @@
+from re import sub
+tab = '\t'
+rb = '{'
+lb = '}'
+
+def make_team_json(status, members):
+    json = f'''{tab}{tab}"status": "{status}",
+'''
+    for name, champion in members:
+        json += \
+f'''{tab}{tab}"{name}": {rb}
+{tab}{tab}{tab}"position": "",
+{tab}{tab}{tab}"champion": "{champion}"
+{tab}{tab}{lb},
+'''
+    return json[:-1]
+
+teamA = []; teamB = []
+for row in range(15):
+    raw = input()
+    if row == 0:
+        date = sub(r"\D", "", raw.split('(')[0])
+        duration = raw.split('(')[1].split(')')[0]
+    elif row == 2:
+        stateA = "WIN" if "승" in raw else "LOSE"
+    elif 3 <= row <= 7:
+        teamA.append(raw.split(': '))
+    elif row == 9:
+        stateB = "WIN" if "승" in raw else "LOSE"
+    elif 10 <= row <= 14:
+        teamB.append(raw.split(': '))
+
+with open(f"data/{date}.json", "w") as f:
+    f.write(f'''{rb}
+{tab}"length": "{duration}",
+{tab}"teamA": {rb}
+{make_team_json(stateA, teamA)}
+{tab}{lb},
+{tab}"teamB": {rb}
+{make_team_json(stateB, teamB)}
+{tab}{lb}
+{lb}''')
