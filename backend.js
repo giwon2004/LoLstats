ed716a7a3c3a067761ce5d0f1b5938cd5c70201e
giwon2004 <giwons04@naver.com>
2023-10-08 15:11:39 +0900

    test

---
+++ backend.js
@@ -0,0 +1,39 @@
+function read_files(dirname, process) {
+	fs.readdir(dirname, function(err, filenames) {
+		if (err) return;
+		filenames.forEach(function(filename) {
+			fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+				if (err) return;
+				process(filename, content);
+			});
+		});
+	});
+}
+
+function player_statistics(name){
+	var win = 0;
+	var lose = 0;
+	var position = {"top": 0, "jgl": 0, "mid": 0, "bot": 0, "sup": 0};
+	read_files('data/', (filename, content) => {
+		if (name in content["teamA"]) {
+			if (content["teamA"]["status"] == "WIN") win++;
+			else lose++;
+			position[content["teamA"][name]["position"]]++;
+		}
+		else if (name in content["teamB"]) {
+			if (content["teamB"]["status"] == "WIN") win++;
+			else lose++;
+			position[content["teamA"][name]["position"]]++;
+		}
+	});
+	return {"win": win, "lose": lose, "total": win + lose, "position": position};
+}
+
+
+var names = [];
+fetch('player_info.json').then(
+	(res) => res.text()
+).then(
+	(filecontents) => console.log(filecontents)
+);
+document.getElementById("main").innerHTML = names;
