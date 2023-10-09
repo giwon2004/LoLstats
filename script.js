b4ffc56872b5789f370b6b1af4ec1f9d1f222ff9
giwon2004 <giwons04@naver.com>
2023-10-09 09:22:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,27 +9,28 @@ function read_file(num) {
 }
 
 function read_data() {
-	var num = 1;
+	var num = 0;
 	var res = {};
 	var data = {};
 	while (true) {
+		num++;
 		data = read_file(num);
 		if(data)
 			res[num] = data;
 		else
 			break;
-		num++;
 	}
-	return res;
+	return {"data": res, "total": num};
 }
 
-function player_statistics(name){
+function player_statistics(name, data){
 	var win = 0;
 	var lose = 0;
 	var team = {};
 	var position = {"top": 0, "jgl": 0, "mid": 0, "bot": 0, "sup": 0};
 	var champion = {};
-	read_files((content) => {
+	for (var i = 1; i <= data["total"]; i++) {
+		content = data["res"][i];
 		if (name in content["teamA"])
 			team = content["teamA"];
 		else if (name in content["teamB"])
@@ -49,8 +50,7 @@ function player_statistics(name){
 			champion[player["champion"]]["lose"]++;
 		}
 		position[player["position"]]++;
-
-	});
+	}
 	return {"win": win, "lose": lose, "total": win + lose, "position": position, "champion": champion};
 }
 
@@ -61,8 +61,8 @@ function get_player_list() {
 
 function make_list(data) {
 	var html = "<ul>"
-	for (name in Object.keys(data)) {
-		pstat = player_statistics(name);
+	for (name in Object.keys(get_player_list)) {
+		pstat = player_statistics(name, data);
 		html += "\n\t<li>" + name + ":" + pstat["win"] + "</li>"
 	}
 	html += "\n</ul>";
@@ -70,7 +70,6 @@ function make_list(data) {
 }
 
 var names = [];
-var battle_history = read_data();
-console.log(battle_history);
-
+data = read_data();
+console.log(make_list(data));
 }
